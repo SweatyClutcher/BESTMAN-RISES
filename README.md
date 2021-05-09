441384749b26ee1c7e7c649d42d4588316e7ab3b
SweatyClutcher <74849607+SweatyClutcher@users.noreply.github.com>
2021-05-09 13:30:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# BESTMAN-RISES
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ Umbrella.js
@@ -0,0 +1,23 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("Walking Frame/walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+        this.image2 = loadImage("bat/batman.png");
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        if(frameCount >= 200){
+            image(this.image2, pos.x, pos.y + 70, 300, 300);
+        }
+        else{
+            image(this.image,pos.x,pos.y+70,300,300);
+        }
+    }
+}
